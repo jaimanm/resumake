bafd9d785cb9791496512d1088a0b349fb5d1125
Jaiman Munshi <jaimancoding@gmail.com>
2026-08-21 17:21:10 -0400

    feat: add instant local Google Drive sync to dev.js

--- src-cli/src/commands/dev.js
+++ src-cli/src/commands/dev.js
@@ -1,6 +1,8 @@
 const chokidar = require('chokidar');
 const execa = require('execa');
 const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = async function devCommand() {
   console.log(chalk.cyan('Starting live-development server...'));
@@ -12,14 +14,27 @@ module.exports = async function devCommand() {
 
   watcher
     .on('ready', () => console.log(chalk.green('Watching for changes in .tex files...')))
-    .on('change', async path => {
-      console.log(chalk.yellow(`\nFile ${path} has been changed. Rebuilding...`));
+    .on('change', async fileChanged => {
+      console.log(chalk.yellow(`\nFile ${fileChanged} has been changed. Rebuilding...`));
       try {
         await execa('./build_all.sh');
         console.log(chalk.green('Build successful!'));
+        
+        console.log(chalk.cyan('Syncing directly to Google Drive locally...'));
+        const actionPath = path.join(process.cwd(), '.github/workflows/build-resume.yml');
+        let driveFolder = 'Resumes';
+        
+        if (fs.existsSync(actionPath)) {
+            const actionContent = fs.readFileSync(actionPath, 'utf8');
+            const driveMatch = actionContent.match(/rclone sync PDF_Exports\/ gdrive:(.*?)\//);
+            if (driveMatch) driveFolder = driveMatch[1];
+        }
+        
+        await execa('rclone', ['sync', 'PDF_Exports/', `gdrive:${driveFolder}/`]);
+        console.log(chalk.green(`⚡️ Instant sync to Google Drive (${driveFolder}) complete!`));
+        
       } catch (error) {
-        console.log(chalk.red('Build failed! See build.log for details.'));
-        const fs = require('fs');
+        console.log(chalk.red('Build or sync failed! See build.log for details.'));
         fs.writeFileSync('build.log', (error.stdout || '') + '\n' + (error.stderr || ''));
       }
     });
