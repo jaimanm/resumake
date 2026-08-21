ba1f3485e29d3edd3e28c55ca157e7a72e5e1b9c
Jaiman Munshi <jaimancoding@gmail.com>
2026-08-21 15:49:18 -0400

    fix: explicit font file lookups and nonstopmode for latexmk

--- build_all.sh
+++ build_all.sh
@@ -3,13 +3,13 @@
 mkdir -p PDF_Exports
 
 echo "Building Resume..."
-latexmk -xelatex -jobname="Awesome_CV_Resume" -output-directory="PDF_Exports" resume.tex
+latexmk -xelatex -interaction=nonstopmode -halt-on-error -jobname="Awesome_CV_Resume" -output-directory="PDF_Exports" resume.tex
 
 echo "Building CV..."
-latexmk -xelatex -jobname="Awesome_CV_Full" -output-directory="PDF_Exports" cv.tex
+latexmk -xelatex -interaction=nonstopmode -halt-on-error -jobname="Awesome_CV_Full" -output-directory="PDF_Exports" cv.tex
 
 echo "Building Cover Letter..."
-latexmk -xelatex -jobname="Awesome_CV_CoverLetter" -output-directory="PDF_Exports" coverletter.tex
+latexmk -xelatex -interaction=nonstopmode -halt-on-error -jobname="Awesome_CV_CoverLetter" -output-directory="PDF_Exports" coverletter.tex
 
 # Clean up auxiliary build files
 find PDF_Exports -type f ! -name "*.pdf" -delete
