91436f240d6f58ff90a1b8fd90e5207805baf8c5
Jaiman Munshi <jaimancoding@gmail.com>
2026-08-23 11:54:35 -0400

    fix: clean up latexmk build directory

--- build_all.sh
+++ build_all.sh
@@ -4,3 +4,6 @@ echo "Building Jake's Resume..."
 latexmk -pdf -interaction=nonstopmode -halt-on-error -jobname="Jakes_Resume" -output-directory="PDF_Exports" resume.tex
 find PDF_Exports -type f ! -name "*.pdf" -delete
 echo "Done! Your PDFs are organized in the PDF_Exports directory."
+
+# Clean up auxiliary build folders to keep the workspace clean
+rm -rf *_build
